f6a1ad42a2a9fd23ee3326c1a5920c9ca7be4a9a
GeorgeZane <genchell@aliyun.com>
2020-05-11 18:18:30 +0800

    增加键盘监听事件

--- tank-zane/src/com/mashibing/tank/TankFrame.java
+++ tank-zane/src/com/mashibing/tank/TankFrame.java
@@ -2,6 +2,8 @@ package com.mashibing.tank;
 
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -19,6 +21,10 @@ public class TankFrame extends Frame {
 		setTitle("tank war");
 		// 设置为可见
 		setVisible(true);
+		
+		//添加键盘监听事件
+		this.addKeyListener(new MyKeyListener());
+		
 		// 添加监听器，监听关闭事件
 		addWindowListener(new WindowAdapter() {
 
@@ -41,6 +47,28 @@ public class TankFrame extends Frame {
 		g.fillRect(x, y, 50, 50);
 		x += 10;
 		y += 10;
+	}
+
+	/**  
+	 * @Title: TankFrame.java
+	 * @Package com.mashibing.tank
+	 * @Description: 添加键盘的监听
+	 * @author ZhaoJianQiang
+	 * @date 2020-05-11 18:09 
+	 * @version V1.0  
+	 */
+	class MyKeyListener extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("key pressed!");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("key released!");
+		}
+
 	}
 
 }
